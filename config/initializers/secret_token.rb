0d7bbcbca06cd799be5d3e1a8c2f2f787bce1895
Nishant Chandra <nishantchandra1@gmail.com>
2013-11-22 16:12:55 -0800

    start new rails file and add in rspec instead of test unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CampusConnect::Application.config.secret_key_base = '485b854e4cf3bf20556bc0e052361299e9605a54c475e8d6d61b20ea5933b5ec27b8d73ea42343b961d6e8810a1a8183530203bc529d42ddf3e3bb8389a1d603'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CampusConnect::Application.config.session_store :cookie_store, key: '_Campus_Connect_session'
