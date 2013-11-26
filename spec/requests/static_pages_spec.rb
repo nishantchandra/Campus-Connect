5b93a53bba9cab3459ad2ebcb9104b80bd31b5c5
Nishant Chandra <nishantchandra1@gmail.com>
2013-11-26 09:11:18 -0800

    add capybara and passed failing tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,4 +5,10 @@ require 'spec_helper'
     visit '/static_pages/home'
     expect(page).to have_content('Campus Connect')
   end
+
+  it "should have the author's name" do
+    visit '/static_pages/home'
+    expect(page).to have_content('Nishant')
  end
+end
+
