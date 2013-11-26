1fca15bb729945a2af3871d927d1729c7cacb04c
Nishant Chandra <nishantchandra1@gmail.com>
2013-11-26 08:32:07 -0800

    add spec files

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+ describe "Home page" do
+  it "should have the content 'Sample App'" do
+    visit '/static_pages/home'
+    expect(page).to have_content('Campus Connect')
+  end
+ end
