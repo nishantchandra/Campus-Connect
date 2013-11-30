c7e85ace96fe90a2a957d0764177fb70bed7a425
Nishant Chandra <nishantchandra1@gmail.com>
2013-11-30 00:53:44 -0800

    rspec tests for static pages controller

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,13 +1,12 @@
 require 'spec_helper'
 
  describe "Home page" do
+  before { visit root_path }
   it "should have the content 'Sample App'" do
-    visit '/static_pages/home'
     expect(page).to have_content('Campus Connect')
   end
 
   it "should have the author's name" do
-    visit '/static_pages/home'
     expect(page).to have_content('Nishant')
  end
 end
