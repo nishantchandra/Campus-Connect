2eedf8189cac18f0c340753716a57e448d97701a
Nishant Chandra <nishantchandra1@gmail.com>
2013-12-12 14:12:53 -0800

    add in email to strong params required input

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -32,6 +32,6 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:title, :body, :university)
+    params.require(:post).permit(:title, :body, :email, :university)
   end
 end
