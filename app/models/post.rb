f6f7a141928530bded97b5e6ea86f1414b40c469
Nishant Chandra <nishantchandra1@gmail.com>
2013-12-12 14:11:21 -0800

    fixed bug with mandatory validation of university, changed it to email

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-   validates :title, :university, presence: true
+   validates :title, :email, presence: true
    validates :body, presence: true, length: { maximum: 200 }
    has_many :comments
 end
